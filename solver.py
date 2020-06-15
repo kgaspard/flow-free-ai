ebc2192b2c8167afbddac7c5dd3d0c792d8ce07b
kprogramm <gitsauce1>
2020-06-15 18:29:23 +0100

    update flow image

--- solver.py
+++ solver.py
@@ -3,22 +3,22 @@ from agent import SearchAgent,GameStateAgent,QLearningAgent
 import util
 import threading
 
-game = game.generate_game('medium') # tiny, small, medium, large
+game = game.generate_game('small') # tiny, small, medium, large
 qLearningAgent = QLearningAgent(game)
 
 # x = threading.Thread(target=agent.solve)
 # x.start()
 
 #### Search-based solving:
-# agent = SearchAgent(game)
-# agent.solve_recursively()
-# print(util.get_duration(game.start_time,'game solved'))
-# game.draw()
+agent = SearchAgent(game)
+agent.solve_recursively()
+print(util.get_duration(game.start_time,'game solved'))
+game.draw()
 
 ### Reinforcement learning-based solving:
-state,result = qLearningAgent.playGame(reset_game=False, draw=False)
-# print(result)
-learn = qLearningAgent.learn()
-print(util.get_duration(game.start_time,'game solved'))
-print(qLearningAgent.adopt_policy(draw=True))
+# state,result = qLearningAgent.playGame(reset_game=False, draw=False)
+# # print(result)
+# learn = qLearningAgent.learn()
+# print(util.get_duration(game.start_time,'game solved'))
+# print(qLearningAgent.adopt_policy(draw=True))
 
